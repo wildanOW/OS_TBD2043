4d6f5dbab16bce50b418ea11846dfcaeaec5fbfc
Wildan <wildan.owais2904@gmail.com>
2025-10-16 14:55:42 +0800

    initial commit

---
+++ 25_10_09-Task1-2-3/Task1.c
@@ -0,0 +1,20 @@
+#include<fcntl.h>
+#include<unistd.h>
+#include<stdio.h>
+int main(int argc, char *argv[]){
+	FILE *fp; char ch; int sc=0;
+	fp=fopen(argv[1], "r");
+	if(fp==NULL)
+		printf("unable t open a file", argv[1]);
+	else{
+		while(!feof(fp))
+		{
+			ch=fgetc(fp); if(ch==' ')
+			sc++;
+
+		}
+		printf("no of space %d", sc);
+		printf("\n");
+		fclose(fp);
+	}
+}
---
+++ 25_10_16-Task1-2-3-4/task4.sh
@@ -0,0 +1,7 @@
+echo -n "Enter Two Numbers(with a space): "
+read a b
+temp=$a
+a=$b
+b= $temp
+echo "after swapping: " $a $b
+
